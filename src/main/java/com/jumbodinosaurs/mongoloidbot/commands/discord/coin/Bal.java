a25ac49689b2ecd4e10b93f5efc0e1dece5ab886
Caleb <43940765+walkinglibrary@users.noreply.github.com>
2021-12-27 16:13:31 -0700

    Adjustments - Added Bal as a Command (Does the Same as Balance)
    Signed-off-by: WalkingLibrary <jumboofawarrior@gmail.com>

---
+++ src/main/java/com/jumbodinosaurs/mongoloidbot/commands/discord/coin/Bal.java
@@ -0,0 +1,68 @@
+package com.jumbodinosaurs.mongoloidbot.commands.discord.coin;
+
+import com.jumbodinosaurs.devlib.commands.Command;
+import com.jumbodinosaurs.devlib.commands.MessageResponse;
+import com.jumbodinosaurs.devlib.commands.exceptions.WaveringParametersException;
+import com.jumbodinosaurs.mongoloidbot.coin.UserAccount;
+import com.jumbodinosaurs.mongoloidbot.coin.exceptions.UserQueryException;
+import com.jumbodinosaurs.mongoloidbot.commands.discord.util.IDiscordChatEventable;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.sql.SQLException;
+import java.util.Base64;
+
+public class Bal extends Command implements IDiscordChatEventable
+{
+    
+    private GuildMessageReceivedEvent event;
+    
+    @Override
+    public MessageResponse getExecutedMessage()
+            throws WaveringParametersException
+    {
+        Member member = event.getMember();
+        String uniqueIdentifier = UserAccount.getUniqueIdentifier(member);
+        String discordUserNameBase64 = Base64.getEncoder().encodeToString(uniqueIdentifier.getBytes());
+        
+        try
+        {
+            UserAccount currentUser = UserAccount.getUser(member);
+            
+            String username = event.getMember().getUser().getName();
+            return new MessageResponse(username +
+                                       "'s Balance: " +
+                                       currentUser.getBalance().toString() +
+                                       " " +
+                                       event.getGuild().getEmoteById("916589679518838794").getAsMention());
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+            return new MessageResponse("Error Checking Database");
+        }
+        catch(UserQueryException e)
+        {
+            e.printStackTrace();
+            return new MessageResponse("Account Error");
+        }
+    }
+    
+    @Override
+    public String getHelpMessage()
+    {
+        return "Allows you to check you Coin Balance\nUsage: \n~" + getClass().getSimpleName();
+    }
+    
+    @Override
+    public GuildMessageReceivedEvent getGuildMessageReceivedEvent()
+    {
+        return event;
+    }
+    
+    @Override
+    public void setGuildMessageReceivedEvent(GuildMessageReceivedEvent event)
+    {
+        this.event = event;
+    }
+}
